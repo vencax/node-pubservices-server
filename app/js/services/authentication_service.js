2582d216834514648966127372653c42c88e4bee
Vaclav Klecanda <vencax77@gmail.com>
2014-10-16 20:56:52 +0200

    updated login api url

--- app/js/services/authentication_service.js
+++ app/js/services/authentication_service.js
@@ -2,10 +2,10 @@ angular.module("app").factory('AuthenticationService', function($http) {
   // these routes map to stubbed API endpoints in config/server.js
   return {
     login: function(credentials) {
-      return $http.post('/login', credentials);
+      return $http.post('/api/login', credentials);
     },
     logout: function() {
-      return $http.post('/logout');
+      return $http.post('/api/logout');
     }
   };
 });
