c9dce6bd5742afcb13cb820fa416432ad1f5c506
Vaclav Klecanda <vencax77@gmail.com>
2014-11-01 00:39:06 +0100

    missing semic

--- app/js/filters/table.js
+++ app/js/filters/table.js
@@ -1,7 +1,7 @@
 
 angular.module('app').filter('nicedate', function(){
   return function(date) {
-    var m = moment(date)
+    var m = moment(date);
     return m.format('l') + ', ' + m.format('LT');
   };
 });
