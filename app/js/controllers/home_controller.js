55918b3457548fda285f23a28a3437b9892d7798
Vaclav Klecanda <vencax77@gmail.com>
2014-10-31 22:05:20 +0100

    home view updates

--- app/js/controllers/home_controller.js
+++ app/js/controllers/home_controller.js
@@ -1,33 +1,10 @@
 
 
-var _err_handler = function(err){
-  alert('request failed: ' + err.statusText + '\n\n' + err.data);
-};
-
-angular.module("app").controller('HomeCtrl', function($scope, $rootScope, $location, AuthService, TicketSrvc) {
-
-  $scope.data = [];
-  TicketSrvc.list().success(function(data) {
-    $scope.data = data;
-  });
+angular.module("app").controller('HomeCtrl',
+  function($scope, $rootScope, TicketSrvc) {
 
   TicketSrvc.validtickets().success(function(tickets) {
     $scope.tickets = tickets;
   });
-
-  $scope.buy = function(ticket) {
-    if (AuthService.isLoggedIn()) {
-      TicketSrvc.buy(ticket).success(function(data) {
-        $rootScope.credit -= ticket.amount;
-        $scope.tickets.push(data);
-      }).error(function(err, status){
-        if (status === 400) {
-          alert('Not enough money!');
-        }
-      });
-    } else {
-      $location.path('/login');
-    }
-  };
 
 });
