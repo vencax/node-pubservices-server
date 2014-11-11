0913e26bf24d91027d3e9ffd48c1427195371f0c
Vaclav Klecanda <vencax77@gmail.com>
2014-11-11 22:59:39 +0100

    home page

--- app/js/controllers/home_controller.js
+++ app/js/controllers/home_controller.js
@@ -1,10 +1,28 @@
 
 
-angular.module("app").controller('HomeCtrl',
-  function($scope, $rootScope, TicketSrvc) {
+angular.module("app")
 
-  TicketSrvc.validtickets().success(function(tickets) {
-    $scope.tickets = tickets;
-  });
+.controller('HomeCtrl', function($scope, $rootScope, $timeout, TicketSrvc, AuthService) {
+
+  $scope.tickets = [];
+
+  $scope.updateRemains = function() {
+    var now = moment();
+    $scope.tickets.forEach(function(t){
+      t.remains = (((t.expires - now) / 60000) | 0) + 1;
+    });
+
+    $timeout($scope.updateRemains, 60000);
+  };
+
+  if(AuthService.isLoggedIn()) {
+    TicketSrvc.validtickets().success(function(tickets) {
+      $scope.tickets = tickets;
+      $scope.tickets.forEach(function(t) {
+        t.expires = moment(t.expires);
+      });
+      $scope.updateRemains();
+    });
+  }
 
 });
